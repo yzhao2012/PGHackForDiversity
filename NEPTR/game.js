1b4cf6cb0a5fb102e76a53ad975c1bae96e83382
dmartelly <dmartelly@gmail.com>
2013-08-03 12:49:07 -0700

    Draw function ready!

--- NEPTR/game.js
+++ NEPTR/game.js
@@ -11,9 +11,10 @@ drawHtmlTable = function(height, width) {
 	}
 }
 
-imageHash = {"empty":"8bit_img/lady_rainicorn.png"};
+var imageHash = {"empty":"8bit_img/lady_rainicorn.png"};
+var size = "40px";
 
-loadLevel = function(board) {
+var loadLevel = function(board) {
 	console.log("start loading level");
 	for (var i = 0; i < board.height; i++) {
 		var newRow = document.createElement("tr");
@@ -28,7 +29,7 @@ loadLevel = function(board) {
 				//$(newCol).css("background-image","url('"+imgPath+"')");	
 			}
 			
-			$(newCol).html("i"+i+"j"+j);
+			
 			$(newRow).append(newCol);
 		}
 		$("#gameBoard").append($(newRow));
@@ -37,4 +38,18 @@ loadLevel = function(board) {
 	var size = "40px";
 	$(".gameCell").css("height",size);
 	$(".gameCell").css("width",size);
+}
+
+drawRobot = function(oldLoc, newLoc) {
+	var oldi = oldLoc[0];
+	var oldj = oldLoc[1];
+	var newi = newLoc[0];
+	var newj = newLoc[1];
+	console.log("let's draw!");
+	//delete old robot
+	$("#i"+oldi+"j"+oldj).html('');
+	//put new robot
+	var imgPath = "./8bit_img/neptr.png"; 
+	console.log('<img src="'+imgPath+'" width="'+size+' height="'+size+'">');
+	$("#i"+newi+"j"+newj).html('<img src="'+imgPath+'" width="'+size+' height="'+size+'">');
 }

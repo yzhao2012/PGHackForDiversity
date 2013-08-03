3a3f94bb7190600de4ae1177f5407ed72dcebdec
Yang Zhao <yang.zhao8810@gmail.com>
2013-08-03 15:35:38 -0700

    made a simple change

--- NEPTR/python.js
+++ NEPTR/python.js
@@ -18,11 +18,6 @@ function builtinRead(x) {
 function runit(robot, board) {
    var prog = "robotHealth = " + robot.health;
    prog += "\nrobotLocation = " + robot.newLocation;
-   
-   prog += "\nnorthInfo = 1";
-   prog += "\nsouthInfo = 2";
-   prog += "\neastInfo = 3";
-   prog += "\nwestInfo = 4";
    prog += "\n" + document.getElementById("givenCode").value; 
    prog += "\n" + document.getElementById("userCode").value;
    var mypre = document.getElementById("output");
